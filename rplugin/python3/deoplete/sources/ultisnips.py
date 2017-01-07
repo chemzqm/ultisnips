823c54c5e022ab160d1fc007a98c10e0a802b21f
chemzqm <chemzqm@gmail.com>
2017-01-07 10:31:31 +0800

    fix deoplete source

--- rplugin/python3/deoplete/sources/ultisnips.py
+++ rplugin/python3/deoplete/sources/ultisnips.py
@@ -1,3 +1,4 @@
+import re
 from .base import Base
 
 class Source(Base):
@@ -5,17 +6,22 @@ class Source(Base):
         Base.__init__(self, vim)
 
         self.name = 'ultisnips'
-        self.mark = '[US]'
+        self.mark = '[U]'
         self.rank = 8
 
     def gather_candidates(self, context):
         suggestions = []
+        prog = re.compile(r'(\w+)\.snippets:')
         snippets = self.vim.eval(
             'UltiSnips#SnippetsInCurrentScope()')
-        for trigger in snippets:
+        name = ''
+        for snip in snippets:
+            match = prog.search(snip.get('location'))
+            if match is not None:
+                name = match.group(1)
             suggestions.append({
-                'word': trigger,
-                'menu': self.mark + ' ' + snippets.get(trigger, ''),
+                'word': snip['key'],
+                'menu': '['+ name + ']',
                 'dup': 1
             })
         return suggestions
